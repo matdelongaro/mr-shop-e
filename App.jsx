cd094725e10f6e907eecdf111f61d163438a3a54
matias delongaro <matiasadelongaro@gmail.com>
2024-07-26 19:56:59 -0300

    first commit

---
+++ App.jsx
@@ -0,0 +1,35 @@
+import { useState } from "react";
+import Header from "./src/components/Header";
+import ProductList from "./src/components/ProductList";
+
+function  App() {
+
+  const [allProducts, setAllProducts] = useState([])
+  const[total, setTotal] = useState(0)
+  const [countProducts, setCountProducts] = useState(0)
+  
+  return (
+  <>
+    <Header  
+    allProducts={allProducts} 
+    setAllProducts={setAllProducts}
+    total={total}
+    setTotal={setTotal}
+    countProducts={countProducts}
+    setCountProducts={setCountProducts}
+    />
+    <ProductList 
+    allProducts={allProducts} 
+    setAllProducts={setAllProducts}
+    total={total}
+    setTotal={setTotal}
+    countProducts={countProducts}
+    setCountProducts={setCountProducts}
+    />
+  </>
+  )
+}
+  
+export default App;
+
+
